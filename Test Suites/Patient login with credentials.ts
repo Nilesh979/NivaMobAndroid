921ad5c56d365f4314c3d77258a34abe6dfe0877
vijeta.dongare <vijeta.dongare@QUEBEC01.figmd.in>
2018-10-04 16:42:24 +0530

    initial

---
+++ Test Suites/Patient login with credentials.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Patient login with credentials</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3e2fd84d-88bb-4338-abaf-16ed79f8f857</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/PatientLogin.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>PatientLogin</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations/>
+</TestSuiteCollectionEntity>
